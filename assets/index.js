6f873bace21f633a16d215df6e1cbb9fc246abc5
satoshi <angelsb94@gmail.com>
2020-04-05 20:54:06 -0500

    clase 5 de js

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video, plugins:[
+    new AutoPlay()
+] });
+
+const btnPlay = document.getElementById('play');
+btnPlay.onclick = () => player.togglePlay();
+
+const btnMute = document.getElementById('mute');
+btnMute.onclick = () => player.toggleMute();
+
