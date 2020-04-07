4acb10fc36268ae14d689356751a920be9c7fc47
satoshi <angelsb94@gmail.com>
2020-04-06 20:18:15 -0500

    clases de extenciones de uso de this

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,11 @@ import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins:[
-    new AutoPlay()
-] });
+const player = new MediaPlayer({ 
+    el: video, 
+    plugins:[//new AutoPlay()
+    ] 
+});
 
 const btnPlay = document.getElementById('play');
 btnPlay.onclick = () => player.togglePlay();
